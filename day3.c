5d3d5dc84ccfcc33b20122432e0248cffb29d7d4
Scarlett Parker <95775118+scarlettmparker@users.noreply.github.com>
2024-12-03 18:12:37 +0000

    day 3 optimization
    skip over don'ts

--- day3.c
+++ day3.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include <time.h>
 
 char * input = ")potato():']mul(12,936)"; // example input
 
@@ -12,43 +11,47 @@ int main()
   int total = 0;
   int i = 0;
 
-  while (i < len) {
+  while (i < len){
     /**
      * PART 2
      */
     if (input[i] == 'd' && input[i+1] == 'o' && input[i+2] == 'n' && input[i+3] == '\''
-    && input[i+4] == 't' && input[i+5] == '(' && input[i+6] == ')') {
+    && input[i+4] == 't' && input[i+5] == '(' && input[i+6] == ')'){
       enabled = 0;
       i += 7;
+      while (i < len && !(input[i] == 'd' && input[i+1] == 'o' && input[i+2] == '(' && input[i+3] == ')')){
+        i++;
+      }
+      continue;
     }
-    else if (input[i] == 'd' && input[i+1] == 'o' && input[i+2] == '(' && input[i+3] == ')') {
+    else if (input[i] == 'd' && input[i+1] == 'o' && input[i+2] == '(' && input[i+3] == ')'){
       enabled = 1;
       i += 4;
     }
-    
+        
     /**
-     *PART 1
+     * PART 1
      */
-    if (input[i] == 'm' && input[i+1] == 'u' && input[i+2] == 'l' && input[i+3] == '(') {
+    if (input[i] == 'm' && input[i+1] == 'u' && input[i+2] == 'l' && input[i+3] == '('){
       i += 4;
       int left = 0, right = 0;
 
-      while (i < len && isdigit(input[i])) {
+      while (i < len && isdigit(input[i])){
         left = left * 10 + (input[i++] - '0');
       }
 
-      if (input[i] == ',') {
+      if (input[i] == ','){
         i++;
-        while (i < len && isdigit(input[i])) {
+        while (i < len && isdigit(input[i])){
           right = right * 10 + (input[i++] - '0');
         }
-        if (input[i] == ')' && enabled) {
+        if (input[i] == ')' && enabled){
           total += (left * right);
         }
         i++;
       }
     }
-    else {
+    else{
       i++;
     }
   }
